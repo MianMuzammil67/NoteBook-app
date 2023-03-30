b5b7c8e220e7588591c3b332113e80022eaa2fca
mian muzammil <mianmuzammil30@gmail.com>
2023-03-30 15:34:07 +0500

    changed UI, implemented search view and categories

--- app/src/main/java/com/mianmuzammil/roomapplication/Adapter/RvMainAdapter.java
+++ app/src/main/java/com/mianmuzammil/roomapplication/Adapter/RvMainAdapter.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,20 +15,29 @@ import com.mianmuzammil.roomapplication.Activities.UpdateNotesActivity;
 import com.mianmuzammil.roomapplication.Entity.Notes;
 import com.mianmuzammil.roomapplication.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RvMainAdapter extends RecyclerView.Adapter<RvMainAdapter.viewHolder> {
     Context context;
     List<Notes> list;
+    List<Notes> filteredList;
 
     public RvMainAdapter(Context context, List<Notes> list) {
         this.context = context;
         this.list = list;
+        filteredList = new ArrayList<>(list);
     }
+
+    public void filteredData(List<Notes> filteredList) {
+        this.list = filteredList;
+        notifyDataSetChanged();
+    }
+
     @NonNull
     @Override
     public RvMainAdapter.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.rv_main_view,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.rv_main_view, parent, false);
         return new viewHolder(view);
     }
 
@@ -35,33 +45,33 @@ public class RvMainAdapter extends RecyclerView.Adapter<RvMainAdapter.viewHolder
     public void onBindViewHolder(@NonNull RvMainAdapter.viewHolder holder, int position) {
 
         Notes note = list.get(position);
+//        if (list.size() == 0){
+//            holder.placeholder.setVisibility(View.VISIBLE);
+//        }else {
+            if (note.priority.equals("1")) {
+                holder.priority.setBackgroundResource(R.drawable.yellow_shape);
+            } else if (note.priority.equals("2")) {
+                holder.priority.setBackgroundResource(R.drawable.green_shape);
+            } else if (note.priority.equals("3")) {
+                holder.priority.setBackgroundResource(R.drawable.red_shape);
+            }
 
-        if (note.priority.equals("1") ){
-            holder.priority.setBackgroundResource(R.drawable.yellow_shape);
-        }else if (note.priority.equals("2")){
-            holder.priority.setBackgroundResource(R.drawable.green_shape);
-        }else if (note.priority.equals("3")){
-            holder.priority.setBackgroundResource(R.drawable.red_shape);
+            holder.tittle.setText(note.Tittle);
+            holder.subTittle.setText(note.subTittle);
+            holder.notes.setText(note.Notes);
+            holder.date.setText(note.date);
+            holder.itemView.setOnClickListener(v -> {
+
+                Intent intent = new Intent(context, UpdateNotesActivity.class);
+                intent.putExtra("id", note.id);
+                intent.putExtra("tittle", note.Tittle);
+                intent.putExtra("subTittle", note.subTittle);
+                intent.putExtra("notes", note.Notes);
+                intent.putExtra("priority", note.priority);
+                context.startActivity(intent);
+            });
         }
-
-
-
-        holder.tittle.setText(note.Tittle);
-        holder.subTittle.setText(note.subTittle);
-        holder.notes.setText(note.Notes);
-        holder.date.setText(note.date);
-        holder.itemView.setOnClickListener(v -> {
-
-            Intent intent = new Intent(context, UpdateNotesActivity.class);
-            intent.putExtra("id",note.id);
-            intent.putExtra("tittle",note.Tittle);
-            intent.putExtra("subTittle",note.subTittle);
-            intent.putExtra("notes",note.Notes);
-            intent.putExtra("priority",note.priority);
-            context.startActivity(intent);
-        });
-
-    }
+//    }
 
     @Override
     public int getItemCount() {
@@ -69,15 +79,18 @@ public class RvMainAdapter extends RecyclerView.Adapter<RvMainAdapter.viewHolder
     }
 
     public static class viewHolder extends RecyclerView.ViewHolder {
-        TextView tittle,subTittle,notes,date;
+        TextView tittle, subTittle, notes, date;
+        ImageView placeholder;
         View priority;
+
         public viewHolder(@NonNull View itemView) {
             super(itemView);
-            tittle=itemView.findViewById(R.id.mtvTittle);
-            subTittle= itemView.findViewById(R.id.msubTittle);
+            tittle = itemView.findViewById(R.id.mtvTittle);
+            subTittle = itemView.findViewById(R.id.msubTittle);
             notes = itemView.findViewById(R.id.mtvNotes);
             date = itemView.findViewById(R.id.mdate);
-            priority =itemView.findViewById(R.id.priority);
+            priority = itemView.findViewById(R.id.priority);
+            placeholder = itemView.findViewById(R.id.placeholder);
 
         }
     }
--- app/src/main/java/com/mianmuzammil/roomapplication/MainActivity.java
+++ app/src/main/java/com/mianmuzammil/roomapplication/MainActivity.java
@@ -1,56 +1,133 @@
 package com.mianmuzammil.roomapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.mianmuzammil.roomapplication.Activities.AddNotesActivity;
-import com.mianmuzammil.roomapplication.Activities.SplashActivity;
 import com.mianmuzammil.roomapplication.Adapter.RvMainAdapter;
+import com.mianmuzammil.roomapplication.Entity.Notes;
 import com.mianmuzammil.roomapplication.ViewModel.NotesViewModel;
 import com.mianmuzammil.roomapplication.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-ActivityMainBinding binding;
-NotesViewModel viewModel;
-RvMainAdapter adapter;
+    ActivityMainBinding binding;
+    NotesViewModel viewModel;
+    RvMainAdapter adapter;
+    List<Notes> filterdata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
         Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.teal_200)));
-
-
         viewModel = ViewModelProviders.of(this).get(NotesViewModel.class);
 
-        viewModel.getAllNotes.observe(this,notes -> {
-            adapter= new RvMainAdapter(MainActivity.this,notes);
-            binding.mainRecyclerView.setAdapter(adapter);
-            binding.mainRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        binding.tvDefault.setOnClickListener(view -> {
+            loadDataToCat(1);
+            binding.tvDefault.setBackgroundResource(R.drawable.cat_bg_slected);
+                binding.tvDefault.setTextColor(getResources().getColor(R.color.white));
+                binding.tvLtoH.setBackgroundResource(0);
+                binding.tvLtoH.setTextColor(getResources().getColor(R.color.tv_colour));
+                binding.tvHtoL.setBackgroundResource(0);
+                binding.tvHtoL.setTextColor(getResources().getColor(R.color.tv_colour));
+        });
+        binding.tvLtoH.setOnClickListener(view -> {
+            loadDataToCat(2);
+            binding.tvLtoH.setBackgroundResource(R.drawable.cat_bg_slected);
+            binding.tvLtoH.setTextColor(getResources().getColor(R.color.white));
+            binding.tvDefault.setBackgroundResource(0);
+            binding.tvDefault.setTextColor(getResources().getColor(R.color.tv_colour));
+            binding.tvHtoL.setBackgroundResource(0);
+            binding.tvHtoL.setTextColor(getResources().getColor(R.color.tv_colour));
 
-//            binding.mainRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        });
+        binding.tvHtoL.setOnClickListener(view -> {
+            loadDataToCat(3);
+            binding.tvHtoL.setBackgroundResource(R.drawable.cat_bg_slected);
+            binding.tvHtoL.setTextColor(getResources().getColor(R.color.white));
+            binding.tvLtoH.setBackgroundResource(0);
+            binding.tvLtoH.setTextColor(getResources().getColor(R.color.tv_colour));
+            binding.tvDefault.setBackgroundResource(0);
+            binding.tvDefault.setTextColor(getResources().getColor(R.color.tv_colour));
 
         });
         binding.btnNewNotes.setOnClickListener(v -> {
             startActivity(new Intent(MainActivity.this, AddNotesActivity.class));
         });
+        binding.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                filterNotes(newText);
+
+                return false;
+            }
+        });
+
+        viewModel.getAllNotes.observe(this, notes -> {
+            setAdapter(notes);
+            filterdata = notes;
+//            adapter = new RvMainAdapter(MainActivity.this, notes);
+//            binding.mainRecyclerView.setAdapter(adapter);
+//            binding.mainRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+//            binding.mainRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        });
     }
+    private void filterNotes(String Text) {
+        ArrayList<Notes> filteredList = new ArrayList<>();
+        for (Notes notes : filterdata){
+            if(notes.Tittle.contains(Text)){
+                filteredList.add(notes);
+            }
+        }
+        adapter.filteredData(filteredList);
+    }
+
+    public void loadDataToCat(int i) {
+        if (i == 1){
+            viewModel.getAllNotes.observe(this, notes -> {
+                setAdapter(notes);
+                filterdata = notes;
+            });
+        }else if (i== 2){
+            viewModel.lowToHigh.observe(this, notes -> {
+                setAdapter(notes);
+                filterdata = notes;
+            });
+        }else if (i== 3){
+            viewModel.highToLow.observe(this, notes -> {
+                setAdapter(notes);
+                filterdata = notes;
+            });
+        }
+    }
+    void setAdapter(List<Notes> notes) {
+        adapter = new RvMainAdapter(MainActivity.this, notes);
+        binding.mainRecyclerView.setAdapter(adapter);
+        binding.mainRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+    }
+
 
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
@@ -66,4 +143,5 @@ RvMainAdapter adapter;
 //        }
 //        return super.onOptionsItemSelected(item);
 //    }
+
 }
